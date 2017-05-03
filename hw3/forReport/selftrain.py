15629a01b73aeed10fc9db6c5d033e9d3e6eb683
Yang-de Chen <yongde0108@gmail.com>
2017-05-03 11:26:25 +0800

    semi-supervised

---
+++ hw3/forReport/selftrain.py
@@ -0,0 +1,136 @@
+import sys
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+
+def read_train_data(fname):
+    x_train = []
+    y_train = []
+    with open(fname) as f:
+        # skip the first row
+        next(f)
+        for line in f:
+            label, features = line.strip().split(',')
+            x_train.append(features.split())
+            y_train.append(label)
+    x_train = np.array(x_train, dtype=np.float)
+    x_train = (x_train - x_train.mean()) / x_train.std()
+    y_train = np.array(y_train, dtype=np.int)
+    return x_train, y_train
+
+batch_size = 128
+num_classes = 7
+epochs = 25 
+
+# input image dimensions
+img_rows, img_cols = 48, 48
+
+# the data, shuffled and split between train and test sets
+x_train, y_train = read_train_data(sys.argv[1])
+x_test, y_test = read_train_data(sys.argv[2])
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+num = x_train.shape[0]
+num_label = num // 2
+num_unlabel = num - num_label
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape,
+                 name='testing'))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Dropout(0.3))
+
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adamax(),
+              metrics=['accuracy'])
+
+model.fit(x_train[:num_label], y_train[:num_label],
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+ans = model.predict_classes(x_train[num_label:num], batch_size = batch_size, verbose = 0)
+y_train[num_label:num] = keras.utils.to_categorical(ans, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape,
+                 name='testing'))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Dropout(0.3))
+
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adamax(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
